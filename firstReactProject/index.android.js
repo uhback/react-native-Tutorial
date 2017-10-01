268378b1f086a1ffbd2c9760e922c81df464ad7c
Jayden Back <gmouse03@gmail.com>
2017-10-02 00:23:24 +1300

    ListView used

--- firstReactProject/index.android.js
+++ firstReactProject/index.android.js
@@ -8,12 +8,13 @@ import {
 import Component1 from './src/components/Component1/Component1';
 import Component2 from './src/components/Component2/Component2';
 import Component3 from './src/components/Component3/Component3';
+import Component4 from './src/components/Component4/Component4';
 
 export default class firstReactProject extends Component{
   render(){
     return(
       <View>
-        <Component3 />
+        <Component4 />
       </View>
     );
   }
